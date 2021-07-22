080c54ec2d4756bb1223e41fdfcfdc2f05f96d0c
hongry <hongry18@gmail.com>
2021-07-22 22:35:23 +0900

    체스판 어려운건 아닌데 생각하는데 시간이 걸림

--- go/bj/level11/1018/main.go
+++ go/bj/level11/1018/main.go
@@ -13,7 +13,9 @@ var (
 
 func main() {
 	defer w.Flush()
-	var N, M int
+	var N, M, min int
+
+	min = 100000000
 
 	fmt.Fscanf(r, "%d %d\n", &N, &M)
 	var maps [][]int = make([][]int, N)
@@ -25,12 +27,20 @@ func main() {
 		whiteMap[i] = make([]int, 8)
 		blackMap[i] = make([]int, 8)
 		for j := 0; j < 8; j++ {
-			if j%2 == 0 {
-				whiteMap[i][j] = 1
-				blackMap[i][j] = 0
+			if j == 0 {
+				whiteMap[i][j] = i % 2
 			} else {
-				whiteMap[i][j] = 0
+				if whiteMap[i][j-1] == 0 {
+					whiteMap[i][j] = 1
+				} else {
+					whiteMap[i][j] = 0
+				}
+			}
+
+			if whiteMap[i][j] == 0 {
 				blackMap[i][j] = 1
+			} else {
+				blackMap[i][j] = 0
 			}
 		}
 	}
@@ -48,5 +58,47 @@ func main() {
 		}
 	}
 
-	// fmt.Println(maps)
+	for i := 0; i <= N-8; i++ {
+		for j := 0; j <= M-8; j++ {
+			_m := cmp(maps, i, j, whiteMap)
+
+			if _m < min {
+				min = _m
+			}
+
+			if min == 0 {
+				fmt.Fprintln(w, min)
+				return
+			}
+
+			_m = cmp(maps, i, j, blackMap)
+
+			if _m < min {
+				min = _m
+			}
+
+			if min == 0 {
+				fmt.Fprintln(w, min)
+				return
+			}
+		}
+	}
+
+	fmt.Fprintln(w, min)
+}
+
+func cmp(a [][]int, x, y int, b [][]int) int {
+	var ans int = 0
+
+	for i := x; i < x+8; i++ {
+		for j := y; j < y+8; j++ {
+			if a[i][j] == b[i%8][j%8] {
+				continue
+			}
+
+			ans += 1
+		}
+	}
+
+	return ans
 }
